8f810e3569fff2d7211b41bd8c3eacbec93faa26
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-01-10 16:58:21 +0100

    feat(plock): implemented the plock layout with testing

---
+++ src/Plock.js
@@ -0,0 +1,58 @@
+import * as React from "react";
+
+const uuid = () => Math.random().toString(36).substring(2, 12);
+
+export function Plock({ children, className, styles, nColumns = 3, gap = 10 }) {
+  const [columns, setColumns] = React.useState(() => {
+    return Array.from({ length: nColumns }, (e) => []);
+  });
+
+  React.useLayoutEffect(() => {
+    const columnsElements = Array.from({ length: nColumns }, (e) => []);
+
+    React.Children.forEach(children, (child, index) => {
+      const key = uuid();
+      const cloned = React.cloneElement(child, {
+        ...child.props,
+        key: key,
+      });
+
+      columnsElements[index % nColumns].push(cloned);
+    });
+
+    setColumns(columnsElements);
+  }, [children, nColumns, setColumns]);
+
+  const defaultStyles = {
+    mainGrid: {
+      display: "grid",
+      gridTemplateColumns: `repeat(${nColumns}, 1fr)`,
+      gap: gap,
+    },
+    columnGrid: {
+      display: "grid",
+      gridTemplateColumns: "100%",
+      gap: gap,
+    },
+  };
+
+  return (
+    <div
+      data-testid="plock-container"
+      className={className}
+      style={{ styles, ...defaultStyles.mainGrid }}
+    >
+      {columns.map((column, index) => {
+        return (
+          <div
+            data-testid="plock-column"
+            style={defaultStyles.columnGrid}
+            key={index}
+          >
+            {column}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
