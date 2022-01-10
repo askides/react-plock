8a4c2d88d16354028bf6b6aefda1da44fe0903fb
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-01-10 16:59:37 +0100

    docs: implemented a very simple demo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Plock } from "./Plock";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Plock>
+        <div style={{ height: 100, width: "100px", background: "yellow" }}>
+          1
+        </div>
+        <div style={{ height: 200, width: "100%", background: "blue" }}>2</div>
+        <div style={{ height: 150, width: "100%", background: "red" }}>3</div>
+        <div style={{ height: 500, width: "100%", background: "orange" }}>
+          4
+        </div>
+      </Plock>
+    </div>
+  );
 }
 
 export default App;
