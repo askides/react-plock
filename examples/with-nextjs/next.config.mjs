523a1a063c27a2fb80950f577ed640278c7390d1
Renato Pozzi <renato.pozzi.dev@gmail.com>
2024-02-09 00:42:07 +0100

    feat: add nextjs example

---
+++ examples/with-nextjs/next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
